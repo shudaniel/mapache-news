460d3ef8aabeacd3b5eb4b4ecb388cfc98ce2ee7
Daniel Shu <danielshu98@gmail.com>
2018-04-27 16:57:43 -0700

    Initial firebase setup

---
+++ README.md
@@ -0,0 +1,5 @@
+0. Install maven
+
+1. To build: mvn package
+
+2. To compile: java -cp target/timelines-1.0-SNAPSHOT.jar com.timelines.cs48.app.App
---
+++ src/main/java/com/timelines/cs48/app/App.java
@@ -0,0 +1,45 @@
+package com.timelines.cs48.app;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseCredentials;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.auth.oauth2.GoogleCredentials;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) 
+    {
+        System.out.println( "Hello World!" );
+        try {
+          FileInputStream serviceAccount = new FileInputStream("timelines-6d652-firebase-adminsdk-m2lpy-4b6b1dbdbf.json");
+          FirebaseOptions options = new FirebaseOptions.Builder()
+              .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+              .setDatabaseUrl("https://timelines-6d652.firebaseio.com/")
+              .build();
+
+          FirebaseApp.initializeApp(options);
+
+        } catch  (IOException e){
+          System.out.println("Invalid firebase connection");
+        }
+
+        System.out.println( "Hello World!" );
+
+    }
+}
